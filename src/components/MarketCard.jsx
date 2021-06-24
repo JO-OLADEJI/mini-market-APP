0055940b33b118d3d6e2be67c87b2df6e167456b
Joshua Oladeji <dejijolaoluwa@gmail.com>
2021-06-24 20:08:51 +0100

    added fade effect to the market card images

--- src/components/MarketCard.jsx
+++ src/components/MarketCard.jsx
@@ -6,8 +6,16 @@ import location from '../assets/location.png';
 const MarketCard = (props) => {
   return (
     <div id="MarketCard">
-      <div className="images-container">
+      <div className="images">
+        <div className="images-container">
+          <img src="https://weetracker.com/wp-content/uploads/2019/05/Computer-Village-phones.jpg" alt="" />
+          <img src="https://www.sunnewsonline.com/wp-content/uploads/2017/10/computer-village.jpg" alt="" />
+          <img src="https://techpoint.africa/wp-content/uploads/2017/05/computer-village-relocation-40-of-42.jpg" alt="" />
 
+          {/* <img src={props.images[0]} alt="" />
+          <img src={props.images[1]} alt="" />
+          <img src={props.images[2]} alt="" /> */}
+        </div>
       </div>
       <div className="market-details">
         <h1>{props.name}</h1>
